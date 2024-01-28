a1bd452da5046107bd6caa27c5bfbff18a9e97cd
Julien <gardier.julien@gmail.com>
2024-01-28 14:20:22 +0100

    indentations corrected and added a fetch categories for the select categories input

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,9 +1,12 @@
-import { handleEditionPage } from "./edition.js"
+import {handleEditionPage} from "./edition.js"
 
 //*** Gestion de la section login au sein du DOM ***/
 
 //Création d'une variable pour stocker le token
 let token
+
+// Vérifie si le token existe dans le localstorage
+token = localStorage.getItem('token')
 
 //Récupération de la section login  
 const loginSection = document.getElementById("login-section")
@@ -15,7 +18,7 @@ const contactElement = document.getElementById("contact")
 
 // Créattion d'une fonction de regénéraion des élements de la page d'acceuil
 // quand la secion login est affichée à l'écran
-export function generateHomePage(){
+export function generateHomePage() {
     introElement.style.display = "block"
     introElement.style.display = "flex"
     portfolioElement.style.display = "block"
@@ -25,7 +28,7 @@ export function generateHomePage(){
 
 //Récupération du bouton login et création d'un addEventlistener
 export const loginBtn = document.getElementById("link-login")
-loginBtn.addEventListener("click", ()=>{
+loginBtn.addEventListener("click", () => {
     //console.log("clicked on login link")
 
     //Suppression des éléments au clic
@@ -40,15 +43,15 @@ loginBtn.addEventListener("click", ()=>{
     //Fonctionnement optimal du lien Contact 
     //(disparition de la section login et réapparition des sections Intro, Projets, Contacts)
     const linkContact = document.getElementById("link-contact")
-    linkContact.addEventListener("click", ()=>{
-    generateHomePage()
+    linkContact.addEventListener("click", () => {
+        generateHomePage()
     })
 
     //Fonctionnement optimal du lien Projets 
     //(disparition de la section login et réapparition des sections Intro, Projets, Contacts)
     const linkProjects = document.getElementById("link-projects")
-    linkProjects.addEventListener("click", ()=>{
-    generateHomePage
+    linkProjects.addEventListener("click", () => {
+        generateHomePage
     })
 })
 
@@ -57,10 +60,10 @@ export const logoutBtn = document.createElement("li")
 logoutBtn.innerText = "logout"
 loginBtn.insertAdjacentElement("afterend", logoutBtn)
 logoutBtn.style.display = "none"
-logoutBtn.addEventListener("click", ()=>{
+logoutBtn.addEventListener("click", () => {
     window.localStorage.removeItem("token")
     window.location.href = "/index.html"
-    console.log("clicked")
+    //console.log("clicked")
 })
 
 // Récupération de l'élément modal et ajout d'un style pour le cacher
@@ -78,55 +81,56 @@ let passwordInput = document.getElementById("passwordInput")
 
 
 // Fonction qui permet de récupérer le token de l'utilisateur et de le stocker dans le localstorage
-export async function userLogin(){
+export async function userLogin() {
     const responseLogin = await fetch("http://localhost:5678/api/users/login", {
-    method: 'POST', 
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-    }, 
-    body: JSON.stringify({
-        email: emailInput.value,
-        password: passwordInput.value,
-    })
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: emailInput.value,
+            password: passwordInput.value,
+        })
     })
 
     if (responseLogin.ok) {
-     const dataLogin = await responseLogin.json()
-    //console.log(dataLogin)
-     token = dataLogin.token
-        if(token){
-        //Stocke le token dans le localstorage
-        window.localStorage.setItem("token", token)
-        console.log("Token stored in localStorage:", token);
+        const dataLogin = await responseLogin.json()
+        //console.log(dataLogin)
+        token = dataLogin.token
+        if (token) {
+            //Stocke le token dans le localstorage
+            window.localStorage.setItem("token", token)
+            location.reload();
+            console.log("Token stocké dans le localstorage")
 
+            // Foncion importée de edition.js qui se charge d'incorporer 
+            // sur la page les éléments d'édition suite au login
+            handleEditionPage()
+            console.log("user is logged in");
 
-        // Foncion importée de edition.js qui se charge d'incorporer 
-        // sur la page les éléments d'édition suite au login
-        handleEditionPage()
-
-    }else {
-        console.log("Token not received. Login failed.")
+        } else {
+            console.log("Token not received. Login failed.")
         }
-    }else{
+    } else {
         // Création d'une ID qui permettra,  une fois rattachée au errorMessage, 
         // de vérifier si errorMessage existe pour ne pas le dupliquer au clic
         // sur le bouton submit
-        const errorMessageId = "login-error-message";
-        const existingErrorMessage = document.getElementById(errorMessageId);
-        
+        const errorMessageId = "login-error-message"
+        const existingErrorMessage = document.getElementById(errorMessageId)
+
         // Si l'erreur n'existe pas, on la crée et on l'insère dans le DOM
         if (!existingErrorMessage) {
 
             // Création du message d'erreur
-            const errorMessage = document.createElement("span");
-            errorMessage.innerHTML = "Vos identifiants sont incorrects.";
-            errorMessage.classList.add("error-msg");
-            errorMessage.id = errorMessageId;
-            
+            const errorMessage = document.createElement("span")
+            errorMessage.innerHTML = "Vos identifiants sont incorrects."
+            errorMessage.classList.add("error-msg")
+            errorMessage.id = errorMessageId
+
             // Insertion du message d'erreur dans le DOM
-            const loginTitle = document.querySelector(".login-title");
-            loginTitle.insertAdjacentElement("afterend", errorMessage);
+            const loginTitle = document.querySelector(".login-title")
+            loginTitle.insertAdjacentElement("afterend", errorMessage)
         }
     }
 }
@@ -137,23 +141,8 @@ export async function userLogin(){
 
 // AddEventlistener sur l'input submit (bouton 'envoyer') du formulaire 'login-form'
 export let loginForm = document.getElementById('login-form')
-loginForm.addEventListener('submit', function (event) {
+loginForm.addEventListener('submit', function(event) {
     //Empêche le recharchement de la page par défaut lors du clic sur le submit
     event.preventDefault()
     userLogin()
 })
-
-console.log("Script loaded and executing.");
-window.onload = function() {
-    console.log('window.onload event');
-    let tokenRetrieve = localStorage.getItem('token');
-    if (tokenRetrieve) {
-      isLoggedIn = true;
-      handleEditionPage();
-    } else {
-      generateHomePage();
-    }
-  };
-
-
-
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,27 +1,40 @@
 import "./login.js"
 import "./edition.js"
-import { handleEditionPage } from './edition.js';
-import { generateHomePage } from "./login.js";
-let token
+import {handleEditionPage} from './edition.js';
+import {generateHomePage} from "./login.js";
+
+
+console.log("Script loaded and executing.");
+
+let token = localStorage.getItem('token');
+if (token) {
+    handleEditionPage();
+    console.log("user is logged in");
+} else {
+    generateHomePage();
+    console.log("user is currently not logged in");
+}
+
+
 // Récupération des données GET /works
 export const response = await fetch("http://localhost:5678/api/works")
 export const data = await response.json()
-    //console.log(data);
+//console.log(data);
 
 //Génération des données sur la page web avec la fonction generateData
 export function generateData(data) {
     //Selection du container gallery
-    const gallery = document.querySelector(".gallery");
+    const gallery = document.querySelector(".gallery")
     //Empêche les doublons lorsque l'on clique sur un filtre
     gallery.innerHTML = ""
     // Création d'une boucle pour récupérer les données de l'API
     for (let i = 0; i < data.length; i++) {
-        const designProjects = data[i]
+         const designProjects = data[i]
 
         // Création d’un élément <article> qui comprendra l'image(imageUrl) et le nom(title)
         const galleryCard = document.createElement("article")
         gallery.appendChild(galleryCard)
-    
+
         // Ajout des images dans <article>
         const imageFromDesignProjects = document.createElement("img")
         imageFromDesignProjects.src = designProjects.imageUrl
@@ -33,12 +46,12 @@ export function generateData(data) {
         galleryCard.appendChild(titleFromDesignProjects)
     }
 }
-    
+
 generateData(data)
 
 //Fonction qui filtre les données par catégorie
-function filterDataByCateory(categoryName){
-    return data.filter(function (designProjects){
+function filterDataByCateory(categoryName) {
+    return data.filter(function(designProjects) {
         return designProjects.category && designProjects.category.name === categoryName
     })
 }
@@ -47,8 +60,8 @@ function filterDataByCateory(categoryName){
 const filterButtons = document.querySelectorAll(".filters__btn")
 
 //Pour chaque bouton de filterButtons au click...
-filterButtons.forEach( btn =>{
-    btn.addEventListener("click", ()=>{
+filterButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
         // la variable categoryName récupère le data.category qui a été rajouté au html (exemple:data-category="Objets")
         let categoryName = btn.dataset.category
         //console.log(`Bouton de la catégorie ${categoryName} clické.`)
@@ -58,52 +71,49 @@ filterButtons.forEach( btn =>{
     })
 })
 
-    //*** Mise en commentaire du code refactorisé au dessus ***/
-    
-    // //Sélection du bouton Objets et ajout d'un addEventListener
-    // const btnObjets = document.querySelector(".objects")
-    // btnObjets.addEventListener("click", function () {
-    //     //console.log("clicked");
-    //     // Si designProjects.category existe dans l'API, vérifier si la propiété name === "Objets". 
-    //     const filteredObjects = data.filter(function (designProjects) {
-    //         return designProjects.category && designProjects.category.name === "Objets"
-    //     });
+//*** Mise en commentaire du code refactorisé au dessus ***/
 
-    //     generateData(filteredObjects);
-    // });
+// //Sélection du bouton Objets et ajout d'un addEventListener
+// const btnObjets = document.querySelector(".objects")
+// btnObjets.addEventListener("click", function () {
+//     //console.log("clicked")
+//     // Si designProjects.category existe dans l'API, vérifier si la propiété name === "Objets". 
+//     const filteredObjects = data.filter(function (designProjects) {
+//         return designProjects.category && designProjects.category.name === "Objets"
+//     })
 
-    //  //Sélection du bouton Appartements et ajout d'un addEventListener
-    //  const btnAppartements = document.querySelector(".flats");
-    //  btnAppartements.addEventListener("click", function () {
-    //      //console.log("clicked");
-    //      // Si designProjects.category existe dans l'API, vérifier si la propiété name === "Appartements". 
-    //      const filteredObjects = data.filter(function (designProjects) {
-    //          return designProjects.category && designProjects.category.name === "Appartements";
-    //      });
- 
-    //      generateData(filteredObjects);
-    //  });   
+//     generateData(filteredObjects)
+// });
 
-    //   //Sélection du bouton Hotels & Restaurants et ajout d'un addEventListener
-    //   const btnHotels = document.querySelector(".hotels");
-    //   btnHotels.addEventListener("click", function () {
-    //       //console.log("clicked");
-    //       // Si designProjects.category existe dans l'API, vérifier si la propiété name === "Hotels & restaurants". 
-    //       const filteredObjects = data.filter(function (designProjects) {
-    //           return designProjects.category && designProjects.category.name === "Hotels & restaurants";
-    //       });
-  
-    //       generateData(filteredObjects);
-    //   }); 
-      
+//  //Sélection du bouton Appartements et ajout d'un addEventListener
+//  const btnAppartements = document.querySelector(".flats")
+//  btnAppartements.addEventListener("click", function () {
+//      //console.log("clicked")
+//      // Si designProjects.category existe dans l'API, vérifier si la propiété name === "Appartements". 
+//      const filteredObjects = data.filter(function (designProjects) {
+//          return designProjects.category && designProjects.category.name === "Appartements"
+//      })
+
+//      generateData(filteredObjects)
+//  })   
+
+//   //Sélection du bouton Hotels & Restaurants et ajout d'un addEventListener
+//   const btnHotels = document.querySelector(".hotels")
+//   btnHotels.addEventListener("click", function () {
+//       //console.log("clicked")
+//       // Si designProjects.category existe dans l'API, vérifier si la propiété name === "Hotels & restaurants". 
+//       const filteredObjects = data.filter(function (designProjects) {
+//           return designProjects.category && designProjects.category.name === "Hotels & restaurants"
+//       })
+
+//       generateData(filteredObjects)
+//   }) 
+
 //Sélection du bouton Tous et ajout d'un addEventListener
-const btnAll = document.querySelector(".all");
-    btnAll.addEventListener("click", function () {
-          //console.log("clicked");
-          generateData(data);
-           }); 
+const btnAll = document.querySelector(".all")
+btnAll.addEventListener("click", function() {
+    //console.log("clicked");
+    generateData(data)
+});
 
 //
-
-
-
